a060e5b91d088e12a183956ad5912937aab310b3
Chung Byung Jae <jbj828@naver.com>
2021-07-18 13:43:01 +0900

    study cpp

--- cpp-tutorial/ComplexNumber.cpp
+++ cpp-tutorial/ComplexNumber.cpp
@@ -3,6 +3,7 @@
 class Complex {
  private:
   double real, img;
+  double get_number(const char* str, int from, int to) const;
 
  public:
   Complex(double real, double img) : real(real), img(img) {}
@@ -21,9 +22,14 @@ class Complex {
   Complex& operator*=(const Complex& c);
   Complex& operator/=(const Complex& c);
 
+  Complex operator+(const char* str);
+
   void println() {
     std::cout << "( " << real << " , " << img << " ) " << std::endl;
   }
+
+  // friend Complex operator+(const Complex& a, const Complex& b);
+  friend std::ostream& operator<<(std::ostream& os, const Complex& c);
 };
 
 Complex Complex::operator+(const Complex& c) const {
@@ -71,10 +77,75 @@ Complex& Complex::operator/=(const Complex& c) {
   return *this;
 }
 
-int main() {
-  Complex a(1.0, 2.0);
-  Complex b(3.0, -2.0);
+Complex Complex::operator+(const char* str) {
+  int begin = 0, end = strlen(str);
+  double str_img = 0.0, str_real = 0.0;
 
-  Complex c = a * b;
-  c.println();
+  int pos_i = -1;
+  for (int i = 0; i != end; i++) {
+    if (str[i] == 'i') {
+      pos_i = i;
+      break;
+    }
+  }
+
+  if (pos_i == -1) {
+    str_real = get_number(str, begin, end - 1);
+
+    Complex temp(str_real, str_img);
+    return (*this) + temp;
+  }
+
+  str_real = get_number(str, begin, pos_i - 1);
+  str_img = get_number(str, pos_i + 1, end - 1);
+
+  if (pos_i >= 1 && str[pos_i - 1] == '-') str_img *= -1.0;
+
+  Complex temp(str_real, str_img);
+  return (*this) + temp;
+}
+
+double Complex::get_number(const char* str, int from, int to) const {
+  bool minus = false;
+  if (from > to) return 0;
+
+  if (str[from] == '-') minus = true;
+  if (str[from] == '-' || str[from] == '+') from++;
+
+  double num = 0.0;
+  double decimal = 1.0;
+
+  bool integer_part = true;
+  for (int i = from; i <= to; i++) {
+    if (isdigit(str[i]) && integer_part) {
+      num *= 10.0;
+      num += (str[i] - '0');
+    } else if (str[i] == '.') {
+      integer_part = false;
+    } else if (isdigit(str[i]) && !integer_part) {
+      decimal /= 10.0;
+      num += ((str[i] - '0') * decimal);
+    } else {
+      break;
+    }
+  }
+
+  if (minus) num *= -1.0;
+
+  return num;
+}
+
+std::ostream& operator<<(std::ostream& os, const Complex& c) {
+  os << "( " << c.real << " , " << c.img << " ) ";
+  return os;
+}
+// Complex operator+(const Complex& a, const Complex& b) {
+//   Complex temp(a.real + b.real, a.img + b.img);
+//   return temp;
+// }
+
+int main() {
+  Complex a(0, 0);
+  a = a + "-1.1 + i3.923";
+  std::cout << "a 의 값은 : " << a << " 이다" << std::endl;
 }
